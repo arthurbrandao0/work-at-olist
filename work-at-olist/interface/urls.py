c6009a54b62d5fde6b5ca226a1a39bd5705401d1
arthurbrandao0 <arthurbm15@gmail.com>
2017-06-24 11:59:29 -0300

    categories and channels basic page web done

--- work-at-olist/interface/urls.py
+++ work-at-olist/interface/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.channel_list),
+    url(r'^channels$', views.channel_list),
+    url(r'^categories$', views.category_list),
 ]
--- work-at-olist/interface/views.py
+++ work-at-olist/interface/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Channel, Category
 
 
 def channel_list(request):
-    return render(request, 'interface/channel_list.html', {})
+    channels = Channel.objects.all
+    return render(request, 'interface/channel_list.html', {'channels': channels})
+
+def category_list(request):
+    categories = Category.objects.all
+    return render(request, 'interface/category_list.html', {'categories': categories})
